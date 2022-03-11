28bfe539de258ea29756e1de79112dcb33c8e440
qdenya <63596498+qdenya@users.noreply.github.com>
2022-03-11 18:21:52 +0100

    upd

--- js/mobile.js
+++ js/mobile.js
@@ -1,17 +1,16 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
 const mMenuBtn = document.querySelector(".header_mobile_open");
 const mMenuBtnClose = document.querySelector(".header_mobile_close");
 const mMenu = document.querySelector(".header_mobile");
 
+var dropdown = document.getElementsByClassName("footer_dropdown-btn");
+var i;
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("actives");
     var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
+    if (dropdownContent.style.display === "flex") {
       dropdownContent.style.display = "none";
     } else {
-      dropdownContent.style.display = "block";
+      dropdownContent.style.display = "flex";
     }
   });
 }
@@ -26,3 +25,4 @@ mMenuBtnClose.addEventListener('click', () => {
   document.querySelector("body").classList.toggle("no-scroll");
   document.querySelector(".header_mobile_back").classList.toggle("header_mobile_back_active");
 });
+
